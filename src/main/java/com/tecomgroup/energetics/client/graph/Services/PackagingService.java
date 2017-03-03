cc209ebb3174c85f87089e91ecb1ce5618987bab
TECOM\uvarov.a <uvarov.a@tecomgroup.ru>
2017-03-03 15:45:08 +0300

    added class DelineateRectangle; begin write PackagingService

--- src/main/java/com/tecomgroup/energetics/client/graph/Services/PackagingService.java
+++ src/main/java/com/tecomgroup/energetics/client/graph/Services/PackagingService.java
@@ -1,21 +1,65 @@
 package com.tecomgroup.energetics.client.graph.Services;
 
+import com.tecomgroup.energetics.client.graph.DelineateRectangle;
 import com.tecomgroup.energetics.client.graph.FullGraph;
+import com.tecomgroup.energetics.client.graph.Params;
 
 import java.util.ArrayList;
 
 public class PackagingService {
-    private final ArrayList<double[]> coordList;
-    private final ArrayList<FullGraph> fullGraphs;
+    private ArrayList<double[]> coordList;
+    private ArrayList<FullGraph> fullGraphs;
+    private Params params;
+    private final ArrayList<DelineateRectangle> delineateRectangles = new ArrayList<DelineateRectangle>();
 
 
-    public PackagingService(ArrayList<double[]> coordList, ArrayList<FullGraph> fullGraphs){
+    public PackagingService(ArrayList<double[]> coordList, ArrayList<FullGraph> fullGraphs, Params params){
 
         this.coordList = coordList;
         this.fullGraphs = fullGraphs;
+        this.params = params;
     }
-    public void PackagingGraphs() {
 
+    public void PackagingGraphs() {
+        this.GetDelineateRectangles();
+        int widthGraph = delineateRectangles.get(0).x2 - delineateRectangles.get(0).x1;
+        for (DelineateRectangle delineateRectangle: this.delineateRectangles) {
+
+        }
+    }
+
+    private void GetDelineateRectangles() {
+        for (int i = 0; i < coordList.size(); i++){
+            double[] coordGraph = coordList.get(i);
+            int minX = (int)coordGraph[0];
+            int minY = (int)coordGraph[1];
+            int maxX = (int)coordGraph[0];
+            int maxY = (int)coordGraph[1];
+            for (int j = 1; j < coordGraph.length / 2; j++){
+                if(minX > coordGraph[2 * j]){
+                    minX = (int)coordGraph[2 * j];
+                }
+                if(minY > coordGraph[2 * j + 1]){
+                    minY = (int)coordGraph[2 * j + 1];
+                }
+                if(maxX < coordGraph[2 * j]){
+                    maxX = (int)coordGraph[2 * j];
+                }
+                if(maxY < coordGraph[2 * j + 1]){
+                    maxY = (int)coordGraph[2 * j + 1];
+                }
+            }
+            delineateRectangles.add(new DelineateRectangle(minX, minY, maxX, maxY));
+        }
+
+        this.GetDelineateRectanglesForFullGraph();
+    }
+
+    private void GetDelineateRectanglesForFullGraph() {
+        for (FullGraph fullGraph: this.fullGraphs) {
+            this.delineateRectangles.add(new DelineateRectangle(fullGraph.leftX, fullGraph.basicY - fullGraph.indent,
+                    fullGraph.rightX, fullGraph.basicY + fullGraph.indent));
+        }
     }
 
 }
--- src/main/java/com/tecomgroup/energetics/client/graph/run/Main.java
+++ src/main/java/com/tecomgroup/energetics/client/graph/run/Main.java
@@ -24,23 +24,22 @@ public class Main {
                 allVertices.add(vertices);
             }
 
-            //int[][] mainMatr = graphUtils.createMatrix(graphList.size());
             ArrayList<Integer> sizeDotList = graphUtils.readDot(String.valueOf(k));
             int indent = 0;
             if (sizeDotList.size() != 0) {
                 indent = 70;
             }
-            //params.meanSpringLength = ((params.height + params.width - indent) / 2) / 2;
-           // double[] coordVertexMainGraph = graphUtils.getCoord(mainMatr, params, params.width / 2,
-            //        (params.height + indent) / 2, 0, 0);
+
             ArrayList<double[]> coordList;
             ArrayList<int[][]> adjacencyMatrixList = new ArrayList<int[][]>();
             adjacencyMatrixList.addAll(graphList);
             CalculateCoordService calculateCoordService = new CalculateCoordService(graphUtils, graphList, params);
             coordList = calculateCoordService.CalculateCoord(indent);
             calculateCoordService.GetCoordFullGraph(coordList);
-            PackagingService packagingService = new PackagingService(coordList, calculateCoordService.fullGraphs);
-            Visualizer visualizer = new Visualizer(params.width, params.height, adjacencyMatrixList, coordList, calculateCoordService.fullGraphs);
+            PackagingService packagingService = new PackagingService(coordList, calculateCoordService.fullGraphs, params);
+            packagingService.PackagingGraphs();
+            Visualizer visualizer = new Visualizer(params.width, params.height,
+                    adjacencyMatrixList, coordList, calculateCoordService.fullGraphs);
             int distance = 0;
             if (sizeDotList.size() != 0) {
                 distance = params.width / sizeDotList.size();
